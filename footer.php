f3eb3ed5279acec54d12f4f705f1ca693bba98cc
Jônatas F. Vieira <jonatasvieira@gmail.com>
2018-04-09 00:55:01 -0300

    Alterações estruturais - Links do TOPO e RODAPÉ foram atualizados; - Lib SCSS de Mixins inserida;

--- footer.php
+++ footer.php
@@ -1,185 +1,179 @@
-<!--FOOTER-->
+<?php // <!--FOOTER--> ?>
 
 <footer class="footer">
-    <div class="footer_bottom_area">
-        <div class="container">
-            <div class="footer_bottom">
-                <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                        <div class="footer_widget">
-                            <h4>Fale Conosco</h4>
-                            <ul class="special_widget">
-                                <li>
-                                    <i class="fa fa-phone fa-fw"></i>
-                                    <a href="#">(44) 9999-9999</a>
-                                    <a href="#">(44) 9999-9999</a>
-                                </li>
-                                <li>
-                                    <i class="fa fa-envelope fa-fw"></i>
-                                    <a href="#">email@email.com.br</a>
-                                </li>
-                                <li>
-                                    <i class="fa fa-map-marker fa-fw"></i>
-                                    <a target="_blank" href="#">Lorem ipsum dolor sit amet, Maringá - PR, 00000-000</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                        <div class="footer_widget">
-                            <h4>Minha Conta</h4>
-                            <ul>
-                                <li><a href="minhaconta"><i class="fa fa-angle-right"></i>Minha Conta</a></li>
-                                <li><a href="minhaconta/pedidos"><i class="fa fa-angle-right"></i>Meus Pedidos</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                        <div class="footer_widget">
-                            <h4>Institucional</h4>
-                            <ul>
-                                <li><a href="sobre"><i class="fa fa-angle-right"></i>Sobre Nós</a></li>
-                                <li><a href="faq"><i class="fa fa-angle-right"></i>Dúvidas Frequentes</a></li>
-                                <li><a href="informacoes/2/trocas-e-devolucoes"><i class="fa fa-angle-right"></i>Trocas e Devoluções</a></li>
-                                <li><a href="informacoes/1/prazos"><i class="fa fa-angle-right"></i>Prazos</a></li>
-                                <li><a class="special abrePolitica" href="termosdeprivacidade-in" target="_blank" role="link"><i class="fa fa-angle-right"></i>Política de Privacidade</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                        <div class="footer_widget">
-                            <h4>Atendimento</h4>
-                            <ul>
-                                <li><span class="floatleft"><i class="fa fa-angle-right"></i> Segunda - Sexta das 8.00 AM - 8.00 PM</span></li>
-                                <li><span class="floatleft"><i class="fa fa-angle-right"></i> Sabado das 8.00 AM - 12.00 PM</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  <div class="footer_bottom_area">
+    <div class="container">
+      <div class="footer_bottom">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_widget">
+              <h4>Fale Conosco</h4>
+              <ul class="special_widget">
+                <li>
+                  <i class="fa fa-phone fa-fw"></i>
+                  <a href="#">(44) 9999-9999</a>
+                  <a href="#">(44) 9999-9999</a>
+                </li>
+                <li>
+                  <i class="fa fa-envelope fa-fw"></i>
+                  <a href="#">email@email.com.br</a>
+                </li>
+                <li>
+                  <i class="fa fa-map-marker fa-fw"></i>
+                  <a target="_blank" href="#">Lorem ipsum dolor sit amet, Maringá - PR, 00000-000</a>
+                </li>
+              </ul>
             </div>
-        </div>
-    </div>
-    <div class="sub-footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <span>Copyright © 2017 , NOME CLIENTE</span>
-                    <ul id="social-icons">
-                        <li><a href="#" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#" class="twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                        <li><a href="#" class="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#" class="youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-12 text-right">
-                    <span>Site criado por</span>
-                    <a href="http://ltxxdesign.com.br/" target="_blank" id="logoltx">
-                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 542 133" style="enable-background:new 0 0 150 37;" xml:space="preserve">
-						<g>
-							<polygon class="cor1" points="123.3,15.1 70.5,67.9 123.3,120.7 133.2,110.9 90.2,67.9 133.2,24.9" />
-							<polygon class="cor1" points="163.3,36.9 148.9,36.9 148.9,97.9 184.7,97.8 184.7,83.4 163.3,83.4" />
-							<polygon class="cor1" points="14.1,124.3 24,134.1 66.9,91.2 109.9,134.1 119.8,124.3 66.9,71.4" />
-							<path class="cor1" d="M527.9,37.2v28.3l-30.3-28.7v28.4l-32.2-0.1v14.3h3.4c1.1,0,2.7,0,4.7,0.1c2.2,0.1,3.8,0.1,4.8,0.1h3.4
-		c-3.1,3.6-7.5,5.4-13.2,5.4c-5.1,0-9.3-1.6-12.6-5c-3.3-3.3-5-7.6-5-12.7c0-5,1.7-9.2,5.1-12.5c3.3-3.3,7.5-5.1,12.5-5.1
-		c6.1,0,11.3,3,15.6,8.9l11.5-8.5c-7.2-9.9-16.3-14.9-27.1-14.9c-8.9,0-16.6,3.1-22.7,9.3c-6.1,6.1-9.3,13.8-9.3,22.7
-		c0,9.1,3,16.8,9.2,22.9c6.1,6.1,13.7,9.1,22.8,9.1c9.9,0,17.9-3.2,23.8-9.7c2.1-2.3,3.9-4.8,5.2-7.5V98H512V69.6l30.3,28.6v-61
-		H527.9z" />
-							<path class="cor1" d="M259.4,48.6l-7-11l-3.1,0l-9.8,16.1l-9.8-16.1H213l0,0.1l-39.3-0.1l-0.3,13.6h17.1v46.6h14.4V51.3h16.5l9.8,16
-		L212,97.9h16.7c4.8-7.7,8.3-13.3,10.8-17l9.9,17.6h3l7-13.3L248,67.2L259.4,48.6z" />
-							<path class="cor1" d="M421.5,75.5c-0.8-4.3-3.2-8.2-7.2-11.4c-1.7-1.4-3.9-2.6-6.4-3.3l-6.2-2h0.1c-2.3-0.8-3.3-2-3.2-3.9
-		c0.1-2,1.2-3.1,3.2-3.3c2.7-0.3,4.8,1.2,6.5,4.4l12.5-7c-4.5-7.9-10.7-11.8-18.4-11.8c-4.8,0-8.9,1.6-12.5,4.7
-		c-2.5,2.3-4.2,5-5.1,8.1V37.1h-34.9v16.8c-1.4-2.8-3.3-5.4-5.7-7.7c-5.8-5.8-13-8.7-21.6-8.8l-18.8-0.1v0.5h0.3l-0.3,0.4v57.8
-		l1.2,2h-1.2V98h18.8c8.6,0,15.8-2.9,21.6-8.7c2.4-2.3,4.3-4.9,5.7-7.7V98h34.9v-9.5c3.5,5.9,9.4,9.2,17.5,9.2
-		c5.3,0,9.8-1.6,13.4-4.7c3.2-2.7,5.1-6.2,5.8-10.6V98h14.3V37.2h-14.3V75.5z M407.3,79.2c-0.2,2.5-1.5,3.9-4,4.2
-		c-1.9,0.3-3.7-0.3-5.2-1.9c-1.4-1.4-2.2-3.1-2.5-5.2l-14.2,1.9c0.3,2,0.7,3.8,1.2,5.5h-18.4v-8.6h15.3V60.7h-15.3v-9.1h20.2
-		c-0.1,0.7-0.2,1.4-0.3,2.2v-0.1c-0.5,6.1,1.8,11.3,7,15.4c1.9,1.6,4.1,2.7,6.4,3.4l6.4,2C406.3,75.5,407.5,77.1,407.3,79.2z" />
-							<polygon class="cor2" points="43,67.2 0,110.2 9.9,120 62.7,67.2 9.9,14.4 0,24.2 43,67.2 	" />
-							<path class="cor3" d="M305.3,98.4h-16.9l-10.9-17.6c-2.5,3.7-6.2,10-11,17.6h-16.9L269,67.3l-18.1-29.6h16.7l9.8,16.1l9.8-16.1h16.7
-		l-18.1,29.5L305.3,98.4L305.3,98.4z" />
-							<polygon class="cor3" points="66.2,63.7 13.4,10.9 23.3,1 66.2,44 109.2,1 119.1,10.9 66.2,63.7" />
-						</g>
-					</svg>
-                    </a>
-                </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_widget">
+              <h4>Minha Conta</h4>
+              <ul>
+                <li><a href="minhaconta.php"><i class="fa fa-angle-right"></i>Minha Conta</a></li>
+                <li><a href="pedidos.php"><i class="fa fa-angle-right"></i>Meus Pedidos</a></li>
+              </ul>
             </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_widget">
+              <h4>Institucional</h4>
+              <ul>
+                <li><a href="sobre.php"><i class="fa fa-angle-right"></i>Sobre Nós</a></li>
+                <li><a href="faq.php"><i class="fa fa-angle-right"></i>Dúvidas Frequentes</a></li>
+                <li><a href="trocas.php"><i class="fa fa-angle-right"></i>Trocas e Devoluções</a></li>
+                <li><a href="prazos.php"><i class="fa fa-angle-right"></i>Prazos</a></li>
+                <li><a class="special abrePolitica" href="politicas.php" target="_blank" role="link"><i class="fa fa-angle-right"></i>Política de Privacidade</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_widget">
+              <h4>Atendimento</h4>
+              <ul>
+                <li><span class="floatleft"><i class="fa fa-angle-right"></i> Segunda - Sexta das 8.00 AM - 8.00 PM</span></li>
+                <li><span class="floatleft"><i class="fa fa-angle-right"></i> Sabado das 8.00 AM - 12.00 PM</span></li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  <div class="sub-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-6 col-xs-12">
+          <span>Copyright © 2017 | Casa do Arquiteto</span>
+          <ul id="social-icons">
+            <li><a href="#" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#" class="twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#" class="linkedin" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#" class="youtube" target="_blank"><i class="fa fa-youtube"></i></a></li>
+          </ul>
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-12 text-right">
+          <span>Site criado por</span>
+          <a href="http://ltxxdesign.com.br/" target="_blank" id="logoltx">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 542 133" style="enable-background:new 0 0 150 37;" xml:space="preserve">
+  						<g>
+  							<polygon class="cor1" points="123.3,15.1 70.5,67.9 123.3,120.7 133.2,110.9 90.2,67.9 133.2,24.9" />
+  							<polygon class="cor1" points="163.3,36.9 148.9,36.9 148.9,97.9 184.7,97.8 184.7,83.4 163.3,83.4" />
+  							<polygon class="cor1" points="14.1,124.3 24,134.1 66.9,91.2 109.9,134.1 119.8,124.3 66.9,71.4" />
+  							<path class="cor1" d="M527.9,37.2v28.3l-30.3-28.7v28.4l-32.2-0.1v14.3h3.4c1.1,0,2.7,0,4.7,0.1c2.2,0.1,3.8,0.1,4.8,0.1h3.4
+  		c-3.1,3.6-7.5,5.4-13.2,5.4c-5.1,0-9.3-1.6-12.6-5c-3.3-3.3-5-7.6-5-12.7c0-5,1.7-9.2,5.1-12.5c3.3-3.3,7.5-5.1,12.5-5.1
+  		c6.1,0,11.3,3,15.6,8.9l11.5-8.5c-7.2-9.9-16.3-14.9-27.1-14.9c-8.9,0-16.6,3.1-22.7,9.3c-6.1,6.1-9.3,13.8-9.3,22.7
+  		c0,9.1,3,16.8,9.2,22.9c6.1,6.1,13.7,9.1,22.8,9.1c9.9,0,17.9-3.2,23.8-9.7c2.1-2.3,3.9-4.8,5.2-7.5V98H512V69.6l30.3,28.6v-61
+  		H527.9z" />
+  							<path class="cor1" d="M259.4,48.6l-7-11l-3.1,0l-9.8,16.1l-9.8-16.1H213l0,0.1l-39.3-0.1l-0.3,13.6h17.1v46.6h14.4V51.3h16.5l9.8,16
+  		L212,97.9h16.7c4.8-7.7,8.3-13.3,10.8-17l9.9,17.6h3l7-13.3L248,67.2L259.4,48.6z" />
+  							<path class="cor1" d="M421.5,75.5c-0.8-4.3-3.2-8.2-7.2-11.4c-1.7-1.4-3.9-2.6-6.4-3.3l-6.2-2h0.1c-2.3-0.8-3.3-2-3.2-3.9
+  		c0.1-2,1.2-3.1,3.2-3.3c2.7-0.3,4.8,1.2,6.5,4.4l12.5-7c-4.5-7.9-10.7-11.8-18.4-11.8c-4.8,0-8.9,1.6-12.5,4.7
+  		c-2.5,2.3-4.2,5-5.1,8.1V37.1h-34.9v16.8c-1.4-2.8-3.3-5.4-5.7-7.7c-5.8-5.8-13-8.7-21.6-8.8l-18.8-0.1v0.5h0.3l-0.3,0.4v57.8
+  		l1.2,2h-1.2V98h18.8c8.6,0,15.8-2.9,21.6-8.7c2.4-2.3,4.3-4.9,5.7-7.7V98h34.9v-9.5c3.5,5.9,9.4,9.2,17.5,9.2
+  		c5.3,0,9.8-1.6,13.4-4.7c3.2-2.7,5.1-6.2,5.8-10.6V98h14.3V37.2h-14.3V75.5z M407.3,79.2c-0.2,2.5-1.5,3.9-4,4.2
+  		c-1.9,0.3-3.7-0.3-5.2-1.9c-1.4-1.4-2.2-3.1-2.5-5.2l-14.2,1.9c0.3,2,0.7,3.8,1.2,5.5h-18.4v-8.6h15.3V60.7h-15.3v-9.1h20.2
+  		c-0.1,0.7-0.2,1.4-0.3,2.2v-0.1c-0.5,6.1,1.8,11.3,7,15.4c1.9,1.6,4.1,2.7,6.4,3.4l6.4,2C406.3,75.5,407.5,77.1,407.3,79.2z" />
+  							<polygon class="cor2" points="43,67.2 0,110.2 9.9,120 62.7,67.2 9.9,14.4 0,24.2 43,67.2 	" />
+  							<path class="cor3" d="M305.3,98.4h-16.9l-10.9-17.6c-2.5,3.7-6.2,10-11,17.6h-16.9L269,67.3l-18.1-29.6h16.7l9.8,16.1l9.8-16.1h16.7
+  		l-18.1,29.5L305.3,98.4L305.3,98.4z" />
+  							<polygon class="cor3" points="66.2,63.7 13.4,10.9 23.3,1 66.2,44 109.2,1 119.1,10.9 66.2,63.7" />
+  						</g>
+  					</svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
-<a href="#" class="scrollup" style="display: inline;"><i class="fa fa-long-arrow-up" aria-hidden="true"></i></a>
+<a href="#" class="scrollup totop" style="display: inline;"><i class="fa fa-long-arrow-up" aria-hidden="true"></i></a>
 
-
-
-
-
-
-
-
-
-<!--SCRIPTS-->
+<?php // <!--SCRIPTS--> ?>
 <script src="assets/js/SmoothScroll.js"></script>
 <script src="assets/js/jquery.form.min.js"></script>
 <script>
-    $(document).ready(function() {
-        // Scroll To Top
-        $(".scrollup").hide();
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 100) {
-                $(".scrollup").fadeIn();
-            } else {
-                $(".scrollup").fadeOut();
-            }
-        });
-        $('.totop').on("click", function() {
-            $("html, body").animate({
-                scrollTop: 0
-            }, 600);
-            return false;
-        });
-        // PRELOADER 	                
-        $(window).load(function() {
-            $('#status').fadeOut();
-            $('#preloader').delay(350).fadeOut('slow');
-            $('body').delay(350).css({
-                'overflow': 'visible'
-            });
-            $(window).scroll();
-        })
-    });
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('header .navbar').outerHeight();
-    $(window).scroll(function(event) {
-        didScroll = true;
-    });
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 200);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        // Make sure they scroll more than delta
-        if (Math.abs(lastScrollTop - st) <= delta)
-            return;
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight) {
-            // Scroll Down
-            $('header').removeClass('nav-down').addClass('nav-up');
-        } else {
-            // Scroll Up
-            if (st + $(window).height() < $(document).height()) {
-                $('header').removeClass('nav-up').addClass('nav-down');
-            }
-        }
-        lastScrollTop = st;
+  $(document).ready(function() {
+    if( !$(".all-wrap").hasClass('home') ){
+      $("#header").parent().find("> section:eq(0)").css("padding-top", ($("#header").height() + 20) + "px" );
     }
+    // Scroll To Top
+    $(".scrollup").hide();
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 100) {
+        $(".scrollup").fadeIn();
+      } else {
+        $(".scrollup").fadeOut();
+      }
+    });
+    $('.totop').on("click", function() {
+      $("html, body").animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
+    // PRELOADER
+    $(window).load(function() {
+      $('#status').fadeOut();
+      $('#preloader').delay(350).fadeOut('slow');
+      $('body').delay(350).css({
+        'overflow': 'visible'
+      });
+      $(window).scroll();
+    })
+  });
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('header .navbar').outerHeight();
+  $(window).scroll(function(event) {
+    didScroll = true;
+  });
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 200);
 
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $('header').removeClass('nav-up').addClass('nav-down');
+      }
+    }
+    lastScrollTop = st;
+  }
 </script>
 </body>
 
